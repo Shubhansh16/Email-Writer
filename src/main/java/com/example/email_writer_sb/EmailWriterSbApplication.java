7908f144d266a1ba918bffc183b9ebd8270d573e
Shubhansh16 <shubhansh77@gmail.com>
2025-01-28 23:28:04 +0530

    "first commit"

---
+++ src/main/java/com/example/email_writer_sb/EmailWriterSbApplication.java
@@ -0,0 +1,15 @@
+package com.example.email_writer_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterSbApplication {
+
+	//key AIzaSyAdXmin9vM0OZXTmDpiFMjiLXU6QY7tVwM
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterSbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/email_writer_sb/controller/EmailGeneratorController.java
@@ -0,0 +1,25 @@
+package com.example.email_writer_sb.controller;
+
+import com.example.email_writer_sb.EmailGeneratorService;
+import com.example.email_writer_sb.EmailRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    public EmailGeneratorController(EmailGeneratorService emailGeneratorService) {
+        this.emailGeneratorService = emailGeneratorService;
+    }
+
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response  = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
