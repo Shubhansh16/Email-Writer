c1650bfa4d529cd338e8a72a7ae070137c887b21
Shubhansh Mishra <shubhansh77@gmail.com>
2025-01-28 23:59:15 +0530

    Update EmailWriterSbApplication.java

--- src/main/java/com/example/email_writer_sb/EmailWriterSbApplication.java
+++ src/main/java/com/example/email_writer_sb/EmailWriterSbApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailWriterSbApplication {
 
-	//key AIzaSyAdXmin9vM0OZXTmDpiFMjiLXU6QY7tVwM
+	//key
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailWriterSbApplication.class, args);
